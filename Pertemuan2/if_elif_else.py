6c7ba67f7fbafdf77e73b606e558bc82bd7370fa
alif462 <56670322+alif462@users.noreply.github.com>
2019-10-17 14:25:50 +0700

    Add files via upload

---
+++ Pertemuan2/if_elif_else.py
@@ -0,0 +1,10 @@
+umur = 10
+
+if 0 < umur < 5:
+    print("umur masih balita")
+elif 5 < = umur < 12:
+    print("Umur masih anak-anak")
+elif 12 < = umur < 19:
+    print("Umur masih remaja")
+elif
+    print("Umur sudah renta")
